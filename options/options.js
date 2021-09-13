3f6b8cf6e2c19b08b5ef90642cedcf0c3992738a
Crowdbotics <team@crowdbotics.com>
2021-09-13 15:05:08 +0000

    Initial commit.

---
+++ options/options.js
@@ -0,0 +1,6 @@
+export const globalOptions = {
+  name: "tst_daniel_fimp_30504Identifier",
+  url: "https://tst_daniel_fimp_30504Identifier.botics.co",
+  api: "https://tst_daniel_fimp_30504Identifier.botics.co/api/v1"
+}
+export const modulesOptions = {}
